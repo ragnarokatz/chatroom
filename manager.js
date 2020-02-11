0c9ed7ce93441efaa51b94a26b208ccbbd3f958f
Bo Wei Yao <bwyao@myseneca.ca>
2020-02-11 17:59:33 -0500

    add name generation logic to utils

---
+++ manager.js
@@ -0,0 +1,4 @@
+// read in reference data for utils
+// read in nouns, verbs, adjs
+
+// provide routes for normal database usage
--- utils.js
+++ utils.js
@@ -1,5 +1,25 @@
 // utils.js
 
-module.exports.generateName = function() {
+data = {
+}
 
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+module.exports.importReferenceData = function (data) {
+    this.data = data;
+}
+
+module.exports.generateName = function () {
+
+    let name = "";
+    for (key in data) {
+        index = generateRandomNumber(0, data[key].length);
+        name += data[key][index];
+    }
+
+    name += generateRandomNumber(0, 1000);
+    return name;
 }
